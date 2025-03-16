af95445baa9e2b99da619c230310d57ae11404f2
RyoFunai <s24s1040du@s.chibakoudai.jp>
2025-03-17 04:09:12 +0900

    refactoring

--- launch/compressor.launch.py
+++ launch/compressor.launch.py
@@ -5,18 +5,18 @@ import os
 
 def generate_launch_description():
     package_dir = get_package_share_directory('binary_image_compressor')
-    param_file = os.path.join(package_dir, 'config', 'compressor_params.yaml')
+    param_file = os.path.join(package_dir, 'config', 'publisher_params.yaml')
 
-    # 特に重要：パラメータファイルが存在することを確認
     if not os.path.exists(param_file):
         print(f"警告: パラメータファイルが見つかりません: {param_file}")
 
     return LaunchDescription([
+        # パブリッシャーノード
         Node(
             package='binary_image_compressor',
-            executable='compressor_node',
-            name='compressor_node',
+            executable='compressed_image_publisher',
+            name='compressed_image_publisher',
             parameters=[param_file],
             output='screen',
-        )
+        ),
     ])
